ab7791d6e642bf33d4347aa48717c39950f7d369
Jiaxi Xu <jiahxixu@lawn-128-61-62-250.lawn.gatech.edu>
2021-10-01 04:38:58 -0400

    final submission(json file restored)

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 // abstract library
 import { DrawingCommon } from './common';
 import * as THREE from 'three'
-import { GUI } from 'three/examples/jsm/libs/dat.gui.module'
+//import { GUI } from 'three/examples/jsm/libs/dat.gui.module'
 
 const pi = 3.1415926
-const scale = 0.1
+const scale = 0.08
+const textureLoader = new THREE.TextureLoader()
+const normalTexture = textureLoader.load('normalTexture.jpeg')
 const materialArray = [
     //MeshStandardMaterial
     new THREE.MeshPhongMaterial({ color: 0xffffff}), 
@@ -15,7 +17,15 @@ const materialArray = [
         reflectivity: 0.7,
         flatShading: true,
         wireframe: true
-    }) //MeshStandardMaterial would be dark in this case bc both receiveShadow and castShadow set to true
+    }),
+    new THREE.MeshPhongMaterial({color: 0x2b22a4}),
+    new THREE.MeshStandardMaterial({
+        metalness: 0.7,
+        roughness: 0.2,
+        normalMap: normalTexture,
+        color: 0x2b22a4
+    }),
+    //MeshStandardMaterial would be dark in this case bc both receiveShadow and castShadow set to true
     //https://stackoverflow.com/questions/45958268/lighting-not-working-properly-with-meshlambertmaterial-in-three-js 
 ]
 
@@ -32,230 +42,211 @@ class Drawing extends DrawingCommon {
 	initializeScene(){
         const objectRoot = new THREE.Group();
 
-        //*** Debug ****
-        const gui = new GUI()
-
-        //Ground
-        const plane = new THREE.PlaneGeometry(200,200);
-        const material = new THREE.MeshBasicMaterial( {color: 0xffffff, side: THREE.DoubleSide});
-        plane.rotateX(-Math.PI * 0.5);
-        plane.translate(0, -10 * scale, 0)
-        const ground = new THREE.Mesh(plane, material)
-        //objectRoot.add(ground)
+        //*** design helper ****
+        //const gui = new GUI()
         
-        //Building
-        const building =  this.creatBuilding()
-        building.position.set(0, 0, 0)
-        //objectRoot.add(building)
+        //***** Model ************
+        //Ground
+        const plane = new THREE.PlaneGeometry(10,10);
+        plane.rotateX(-Math.PI * 0.5);
+        plane.translate(0, -2.3, 0)
+        const ground = new THREE.Mesh(plane, materialArray[1])
+        objectRoot.add(ground)
 
         //Protagonist 
-        const protagonist = this.createProtagonist(gui) 
+        const protagonist = this.createProtagonist() 
         protagonist.position.set(0, 0, 0)
+        protagonist.scale.set(scale,scale,scale)
         objectRoot.add(protagonist)
 
-        objectRoot.scale.set(scale,scale,scale)
-        this.scene.add(objectRoot);
-
         const gridhelper = new THREE.GridHelper(10,10)
-        //objectRoot.add(gridhelper)
-        this.scene.add(gridhelper)
+        gridhelper.position.set(0,-2.3,0)
+        objectRoot.add(gridhelper)
 
-       
+        this.scene.add(objectRoot);
+        
         // *** Animation ***
+        document.addEventListener('mousemove', onDocumentMouseMove) 
+
+        let mouseX = 0
+        let mouseY = 0
+
+        let targetX = 0
+        let targetY = 0
+
+        const windowHalfX = window.innerWidth/2
+        const windowHalfY = window.innerHeight/2
+
+        function onDocumentMouseMove(event) {
+            mouseX = (event.clientX - windowHalfX)
+            mouseY = (event.clientY - windowHalfY)
+        }
+
         const clock = new THREE.Clock()
         const tick = () => {
+            targetX = mouseX * 0.005
+            targetY = mouseY * 0.005
+
             const elapsedTime = clock.getElapsedTime()
-            objectRoot.rotation.y = .2 * elapsedTime
-            
-            this.renderer.render(this.scene, this.camera)
+
+            // Update objects
+            objectRoot.rotation.y = .5 * elapsedTime
+            objectRoot.rotation.y += 0.5 * (targetX - objectRoot.rotation.y)
 
             // Call tick again on the next frame
             window.requestAnimationFrame(tick)
         }
-        //tick()
+        tick()
     }
 
-    createProtagonist(gui?: GUI): THREE.Group {
-        const group = new THREE.Group();
-        
-        //[top, bottom, height, radialSeg, x, y, z, rotateX, Y, Z, material]
-        const cylinderDims: number[][] = [
-            [2,2,4,10,      0,8.4,0,    0,0,0,0], //neck
-            [5,10,4,6,      0,6,0,0,    0,0,0,0], //body top
-            [10,6,8,6,      0,0,0,      0,0,0,0], // body bottom
-            [1.5,1,23,4,    -5,-7.4,1.3,    -pi/8,0, 0.3,0], // legL
-            [1.5,1,23,4,    5,-7.4,1.3,     -pi/8,0, -0.3,0], //legR
-            [1,1,3,8,       0,-17,5,        0,0,pi/2,0], //legJoint
-            
-            [1.5,1,10,4,    10.6,3.6,-4.4,      -2.1,-0.5,-1.3,0], //armL1
-            [1.5,1,10,4,    -10.6,3.6,-4.4,     -2.1,0.5,1.3,0], //armR1
-            [3,1.5,10,4,    14.6,7.5,-10,       -1,0,0,0], //armL2
-            [3,1.5,10,4,    -14.6,7.5,-10,       -1,0,0,0], //armR2
+    createProtagonist(): THREE.Group {
+        const ProtagonistGroup = new THREE.Group();
 
-            [0.5,0.5,5,4,       4,20,-8.9,     0,-0.5,-1.4,1], //eyebrowL
-            [0.5,0.5,5,4,       -4,20,-8.9,    0,0.4,1.4,1], //eyebrowR
-            
-            [2,2,3,6,       8.5,3.5,-3.4,     pi/2,0,1.3,1], //screw_armL1
-            [2,2,3,6,       -8.5,3.5,-3.4,     pi/2,0,-1.3,1], //screw_armR1
-            [2.5,2.5,2,6,       14.5,5.5,-6.4,     2,0.4,0.4,1], //screw_armL2
-            [2.5,2.5,2,6,       -14.5,5.5,-6.4,    2,0.4,0.4,1], //screw_armR2
+        const groupNames = ["neck","head",              //primary-secondary pair
+                            "wheel", "wheel_skelenton", 
+                            "body_screwL", "armLeft",
+                            "body_screwR","armRight", 
+                            "body"]
+        const primaryGroupPos = [
+            [0,9,0],
+            [0,-17,5],
+            [8.5,3.5,-3.4],
+            [-8.5,3.5,-3.4],
+            [0,0,0]
         ]
-        for (let i = 0; i < cylinderDims.length; i ++) {
-            const curr: number[] = cylinderDims[i]
-            const geo = new THREE.CylinderGeometry(curr[0], curr[1], curr[2], curr[3])
-            const mesh = new THREE.Mesh(geo, materialArray[curr[10]])
-
-            mesh.position.set(curr[4],curr[5],curr[6])
-            mesh.rotateX(curr[7])
-            mesh.rotateY(curr[8])
-            mesh.rotateZ(curr[9])
-
-            group.add(mesh)
-
-            // GUI
-            const x = gui.addFolder('cylinder' + i)
-            x.add(mesh.position, 'x').min(-20).max(25).step(0.1)
-            x.add(mesh.position, 'y').min(-20).max(25).step(0.1)
-            x.add(mesh.position, 'z').min(-20).max(25).step(0.1)
-            x.add(mesh.rotation, 'x').min(-pi).max(pi).step(0.1)
-            x.add(mesh.rotation, 'y').min(-pi).max(pi).step(0.1)
-            x.add(mesh.rotation, 'z').min(-pi).max(pi).step(0.1)
-            var params = {
-                color: 0xff00ff,
-            };
+        var currJoint
+        const initDim: number[][][] = [
+        //geoType  material  position     rotation        geoParams
+            [ //neck
+            [1,     2,       0,0,0,         0,0,0,         2,2,4,10], //neck
+            ],
+            [ //head
+            [2,     0,       0,9.5,0,        0,0,0,          10,6], //skull
+            [3,     1,       -4,8.5,-9,       0,0,0,         2,8,6], //eyeLeft
+            [3,     1,       4,8.5,-9,       0,0,0,          2,8,6], //eyeRight
+            [1,     1,      4,11.5,-8.9,       0,-0.5,-1.4,    0.5,0.5,5,4], //eyebrowL 
+            [1,     1,      -4,11.5,-8.9,      0,0.4,1.4,      0.5,0.5,5,4], //eyebrowR
+            ],
+        
+            [ //wheel
+            [1,     0,      0,0,0,        0,0,pi/2,       1,1,3,8], //wheel_joint
+            [4,     2,      0,0,0,        0,pi/2,0,       10,2.8,16,100], //tire
+            ],
+            [ //wheel_skelenton
+            [1,     2,      0,-4,0,       0,0,0,          .3,.3,8,10], //wheel skeleton,
+            ],
             
-            x.addColor(params, 'color' )
-                .onChange( function() {mesh.material.color.set(params.color ); } );
+            [ //body_screwL
+            [1,     1,      0,0,0,      pi/2,0,1.3,    2,2,3,6], 
+            ],
+            [ //armL
+            [1,     0,    2.7,0.1,-1,     -2.1,-0.5,-1.3,    1.5,1,9,4], //armL1
+            [1,     1,    6,2,-3,         2,0.4,0.4,         2.5,2.5,2,6], //armL_screw
+            [1,     0,    6.1,4,-6.6,     -1,0,0,            3,1.5,9,4], //armL2
+            ],
+            [ //body_screwR
+            [1,     1,      0,0,0,         pi/2,0,-1.3,        2,2,3,6],
+            ],
+            [ //armR
+            [1,     0,    -2.7,0.1,-1,     -2.1,0.5,1.3,     1.5,1,9,4], //armR1
+            [1,     1,    -6,2,-3,         2,0.4,-0.2,        2.5,2.5,2,6], //armR_screw
+            [1,     0,    -6.1,4,-6.6,     -1,0,0,           3,1.5,9,4], //armR2
+            ],
+            
+            [ //body
+            [1,     3,      0,6,0,0,         0,0,0,         5,10,4,6], //body top
+            [1,     4,      0,0,0,           0,0,0,         10,6,8,6], // bodcoy bottom
+            [1,     0,      -5,-7.4,1.3,    -pi/8,0,0.3,    1.5,1,23,4], // legL
+            [1,     0,      5,-7.4,1.3,     -pi/8,0,-0.3,   1.5,1,23,4], //legR
+            ],
+        ]
+        
+        //draw based on params 
+        for (let i = 0; i < initDim.length; i++) {
+            const group = new THREE.Group()
+            group.name = groupNames[i]
+            //const currGui = gui.addFolder(groupNames[i])
+            for (let j = 0; j < initDim[i].length; j++) {
+                const mesh = this.drawGeo(initDim[i][j])
+                group.add(mesh)
+            }
+            if (i%2 == 0) { //is joint 
+                const pos = primaryGroupPos[i/2]
+                group.position.set(pos[0], pos[1],pos[2])
+                currJoint = group
+                ProtagonistGroup.add(group)
+            } else { //is normal object3D
+                currJoint?.add(group)
+            }
+        }
+        
+        //rotation of rightArm group
+        const body_screwR = ProtagonistGroup.getObjectByName("body_screwR")
+        body_screwR?.children[1].rotation.set(-pi/4, 0, 0)
+
+        //transformation of wheel skeleton (1 -> 8)
+        const wgroup = ProtagonistGroup.getObjectByName("wheel")
+        var templetSkeleton = wgroup.children[2]
+        const angels = range(0, 2* pi, pi/4)
+        for (let i = 1; i < angels.length; i++) {
+            const currSkeleton = templetSkeleton.clone()
+            currSkeleton.rotation.set(angels[i], 0, 0)
+            wgroup?.add(currSkeleton)
+            templetSkeleton = currSkeleton
         }
 
-        
-        //********* head *********
-        // skull
-        const headGroup = new THREE.Group();
-        const tetra = new THREE.TetrahedronGeometry(10,6) //radius, seg
-        const skull = new THREE.Mesh(tetra, materialArray[0])
-        skull.position.set(0,18,0);
-
-        const h = gui.addFolder('head')
-        const x = h.addFolder('skull')
-        x.add(skull.position, 'x').min(-20).max(20).step(0.1)
-        x.add(skull.position, 'y').min(-20).max(20).step(0.1)
-        x.add(skull.position, 'z').min(-20).max(20).step(0.1)
-
-        //Two eyes
-        const eye = new THREE.SphereGeometry(2, 8, 6) //radius, seg
-        const eyeL = new THREE.Mesh(eye, materialArray[0])
-        eyeL.position.set(4,17,-9);
-        
-        const eyeR = eyeL.clone()
-        eyeR.position.set(-4,17,-9);
-        
-        headGroup.add(skull, eyeL, eyeR)
-        group.add(headGroup)
-        //********* End of head *********
-
-        //wheel 
-        const arr = [10,2.8,16,100]
-        const torus = new THREE.TorusGeometry(...arr) 
-        const wheel = new THREE.Mesh(torus, materialArray[2])
-        wheel.position.set(0,-17,5);
-        wheel.rotateY(pi/2)
-        group.add(wheel)
-
-        const y = gui.addFolder('torus')
-        y.add(wheel.position, 'x').min(-20).max(20).step(0.1)
-        y.add(wheel.position, 'y').min(-20).max(20).step(0.1)
-        y.add(wheel.position, 'z').min(-20).max(20).step(0.1)
-
-        return group
+        return ProtagonistGroup
     }
 
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //****************************************************************************---
-    createCylinderMeshes(dims: number[][], gui?: GUI): THREE.Group {
-        const group = new THREE.Group();
+    drawGeo(dim: number[]): THREE.Mesh {
+        const material = materialArray[dim[1]]
         
-        return group;
-    }
-
-
-
-    creatBuilding(gui?: GUI): THREE.Group {
-        const group = new THREE.Group();
-        const l = 10;
-
-        var basedDim =  range(1, 7, 2);
-        const fourLayersPile = new THREE.Group();
-        for (let i = basedDim.length -1; i >= 0; i--) {
-            const corssBox = this.createCrossBoxPile(basedDim[i], l)
-            corssBox.position.set(0, 1.5* l * (basedDim.length -1 -i), 0)
-            fourLayersPile.add(corssBox)
+        var geoParam: number[] = []
+        var geo: THREE.BufferGeometry
+        switch (dim[0]) {
+            case 1: //cylinder 4
+                geoParam = dim.slice(dim.length - 4, dim.length)
+                geo = new THREE.CylinderGeometry(...geoParam) 
+                break;
+            case 2: //tetra 2
+                geoParam = dim.slice(dim.length - 2, dim.length)
+                console.log(geoParam)
+                geo = new THREE.TetrahedronGeometry(...geoParam) 
+                break;
+            case 3: //sphere 3
+                geoParam = dim.slice(dim.length - 3, dim.length)
+                geo = new THREE.SphereGeometry(...geoParam) 
+                break;
+            case 4: //Torus 4
+                geoParam = dim.slice(dim.length - 4, dim.length)
+                geo = new THREE.TorusGeometry(...geoParam) 
+                break;
+            default:
+                geo = new THREE.SphereGeometry(2,16,16) 
         }
+        const mesh = new THREE.Mesh(geo, material)
 
-        basedDim =  range(1, 3, 2);
-        const twoLayersPile = new THREE.Group();
-        for (let i = basedDim.length -1; i >= 0; i--) {
-            const corssBox = this.createCrossBoxPile(basedDim[i], l)
-            corssBox.position.set(0, l * (basedDim.length -1 -i), 0)
-            twoLayersPile.add(corssBox)
-        }
+        mesh.position.set(dim[2],dim[3],dim[4])
+        mesh.rotateX(dim[5])
+        mesh.rotateY(dim[6])
+        mesh.rotateZ(dim[7])
 
-        twoLayersPile.position.set(-l, 0, -l)
+        //GUI
+        // const x = gui
+        // x.add(mesh.position, 'x').min(-20).max(25).step(0.1)
+        // x.add(mesh.position, 'y').min(-20).max(25).step(0.1)
+        // x.add(mesh.position, 'z').min(-20).max(25).step(0.1)
+        // x.add(mesh.rotation, 'x').min(-pi).max(pi).step(0.1)
+        // x.add(mesh.rotation, 'y').min(-pi).max(pi).step(0.1)
+        // x.add(mesh.rotation, 'z').min(-pi).max(pi).step(0.1)
+        // var params = {
+        //     color: 0xff00ff,
+        // };
         
-        const twoLayersPile2 = twoLayersPile.clone()
-        twoLayersPile2.position.set(l, 0, -l)
+        // x.addColor(params, 'color' )
+        //     .onChange( function() {mesh.material.color.set(params.color ); } );
 
-        const twoLayersPile3 = twoLayersPile.clone()
-        twoLayersPile3.position.set(l, 0, l)
-
-        const twoLayersPile4 = twoLayersPile.clone()
-        twoLayersPile4.position.set(-l, 0, l)
-
-        const twoLayersPile5 = twoLayersPile.clone()
-        twoLayersPile5.scale.set(0.5, 3.5, 0.5)
-        twoLayersPile5.position.set(-l/2 + 2, l, l/2)
-        
-        group.add(fourLayersPile,twoLayersPile, twoLayersPile2, twoLayersPile3, twoLayersPile4, twoLayersPile5)
-        return group
-    }
-
-    // num: number of boxes along a single axis, width: box width
-    createCrossBoxPile(num:number, width: number, gui?: GUI): THREE.Group {
-        
-        const group = new THREE.Group();
-
-        //const material = new THREE.MeshStandardMaterial({ color: 0xf7a8cc})
-        num = Math.floor(num/2)
-        const pos: number[] = range(-num * width, num * width, width);
-        const box = new THREE.Mesh(
-            new THREE.BoxGeometry(width, width, width),
-            materialArray[0]
-        )
-
-        for (let i = 0; i < pos.length; i++) {
-            let boxX = box.clone()
-            boxX.position.set(pos[i], 0, 0)
-            group.add(boxX)
-
-            let boxZ = box.clone()
-            boxZ.position.set(0, 0, pos[i])
-            group.add(boxZ)
-        }
-        return group
+        return mesh
     }
 
 	/*
@@ -291,7 +282,7 @@ exec()
 // helper functions ----------------------------------------------
 function range(start: number, end: number, step: number) : number[] {
     var ans = [];
-    for (let i = start; i <= end; i += step) {
+    for (let i = start; i < end; i += step) {
         ans.push(i);
     }
     return ans;
